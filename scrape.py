fd76b50047fb608e09e5c7d49e2affe2764c5e28
Jialiang Shi <jialiang.shi@jialiang.local>
2018-12-26 17:40:56 -0800

    scrape 5 pages with random delay

--- scrape.py
+++ scrape.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import random
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,21 +12,38 @@ driver = webdriver.Chrome('/usr/local/bin/chromedriver')  # Optional argument, i
 # driver = webdriver.Chrome('ANACONDA_INSTALL_DIR/chromedriver-Darwin')
 
 # <span class="taLnk ulBlueLinks" onclick="widgetEvCall('handlers.clickExpand',event,this);">更多</span>
-
-driver.get('https://www.tripadvisor.com.tw/Hotel_Review-g13808671-d306446-Reviews-Grand_Hotel_Taipei-Zhongshan_District_Taipei.html')
+URL = 'https://www.tripadvisor.com.tw/Hotel_Review-g13808671-d306446-Reviews-Grand_Hotel_Taipei-Zhongshan_District_Taipei.html'
+driver.get(URL)
 # driver.find_element_by_class_name("taLnk ulBlueLinks").click()
 # here is where some useful work would typically happen
 # search_box = driver.find_element_by_name('search')
 # search_box.send_keys('wings')
 # search_box.submit()
-#
-# <p class="partial_entry">古色古香的飯店，雖然交通不太方便，但飯店有提供接駁車搭乘往捷運站，職員亦親切有禮。房間舒適，風景好，會再入住</p>
+
 # Selenium hands the page source to Beautiful Soup
+titles = []
+comments = []
+delay = (2, 3)
+# <span class="noQuotes">舊式飯店</span>
+# <p class="partial_entry">古色古香的飯店，雖然交通不太方便，但飯店有提供接駁車搭乘往捷運站，職員亦親切有禮。房間舒適，風景好，會再入住</p>
+# driver.find_element_by_link_text('往下').click()
 
-soup = BeautifulSoup(driver.page_source, 'lxml')
+for i in range(5):
 
-for link in soup.find_all("p", {"class":"partial_entry"}):
-    print(link.text)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    for link in soup.find_all("span", {"class": "noQuotes"}):
+        titles.append(link.text)
+    for link in soup.find_all("p", {"class": "partial_entry"}):
+        comments.append(link.text)
 
+    time.sleep(random.randint(delay[0], delay[1]))  # wait random seconds
+    try:
+        driver.find_element_by_link_text('往下').click()
+    except:
+        break
+
+
+print(len(titles))
+# print(comments)
 input("Press Enter to quit")
 driver.quit() # close browser
