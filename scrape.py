06d19b0b8327ab9aaf4d3d61170f2767bcb2f222
Jialiang Shi <jialiang.shi@jialiang.local>
2019-01-02 11:17:53 -0800

    function

--- scrape.py
+++ scrape.py
@@ -1,49 +1,35 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 import time
 import random
-import requests
-from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome('/usr/local/bin/chromedriver')  # Optional argument, if not specified will search path.
-# or, fill in your ANACONDA_INSTALL_DIR and try this:
-# driver = webdriver.Chrome('ANACONDA_INSTALL_DIR/chromedriver-Darwin')
+def get_data(url):
+    driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+    driver.get(url)
+    titles = []
+    comments = []
+    delay = (1, 2)
 
-# <span class="taLnk ulBlueLinks" onclick="widgetEvCall('handlers.clickExpand',event,this);">更多</span>
-URL = 'https://www.tripadvisor.com.tw/Hotel_Review-g13808671-d306446-Reviews-Grand_Hotel_Taipei-Zhongshan_District_Taipei.html'
-driver.get(URL)
-# driver.find_element_by_class_name("taLnk ulBlueLinks").click()
-# here is where some useful work would typically happen
-# search_box = driver.find_element_by_name('search')
-# search_box.send_keys('wings')
-# search_box.submit()
+    while True:
+        # show whole comments, doesn't work for now
+        # driver.find_element_by_link_text('更多').click()
 
-# Selenium hands the page source to Beautiful Soup
-titles = []
-comments = []
-delay = (2, 3)
-# <span class="noQuotes">舊式飯店</span>
-# <p class="partial_entry">古色古香的飯店，雖然交通不太方便，但飯店有提供接駁車搭乘往捷運站，職員亦親切有禮。房間舒適，風景好，會再入住</p>
-# driver.find_element_by_link_text('往下').click()
-
-for i in range(10):
-
-    soup = BeautifulSoup(driver.page_source, 'lxml')
-    for link in soup.find_all("span", {"class": "noQuotes"}):
-        titles.append(link.text)
-    for link in soup.find_all("p", {"class": "partial_entry"}):
-        comments.append(link.text)
-
-    time.sleep(random.randint(delay[0], delay[1]))  # wait random seconds
-    try:
-        driver.find_element_by_link_text('往下').click()
-    except:
-        break
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        for link in soup.find_all("span", {"class": "noQuotes"}):
+            titles.append(link.text)
+        for link in soup.find_all("p", {"class": "partial_entry"}):
+            comments.append(link.text)
+        time.sleep(random.randint(delay[0], delay[1]))  # wait random seconds
+        # <span class="taLnk ulBlueLinks" onclick="widgetEvCall('handlers.clickExpand',event,this);">更多</span>
+        try:
+            driver.find_element_by_link_text('往下').click()
+        except:
+            break  # break the loop at the last page
+    input("Press Enter to quit")
+    driver.quit()
+    return [titles, comments]
 
 
-print(titles)
-# print(comments)
-input("Press Enter to quit")
-driver.quit() # close browser
+url = 'https://www.tripadvisor.com.tw/Hotel_Review-g13808450-d3856249-Reviews-Taichung_Harbor_Hotel-Wuqi_Taichung.html'
+data = get_data(url)
+print(len(data[0]))
